e7bcc5655ab6a572747470bf5912e7fedbd9a500
7510leslie <80203065+7510leslie@users.noreply.github.com>
2021-03-09 02:44:28 -0500

    Update Project_Aluofu_Leslie.cpp

--- Project_Aluofu_Leslie.cpp
+++ Project_Aluofu_Leslie.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
+
 
 int main()
 {
 	int speedGpu, speedCpu, numberOfcores, menu, performanceScore;
 	
+	double mult;
+	
+	if(speedGpu > 0){
 	
 	cout << "Enter the clock speed (in Megahertz) of their graphic card " << endl;
-	
+}else{
+	cout << "Invalid graphics card speed entered. Program will terminate " << endl;
+}
+
 	cin >> speedGpu;
 	
-	cout << "Enter the clock speed (in Megahertz) of their processor (CPU) " << endl;
+	if(speedCpu > 0){
 	
-	cin >> speedCpu;
+	cout << "Enter the clock speed (in Megahertz) of their processor (CPU) " << endl;
+}else{
+	cout << "Invalid Processor speed entered. Program will terminate " << endl;
+}
+    cin >> speedCpu;
+    
+    if(numberOfcores > 0){
 	
 	cout << "Enter the number of cores that their processor(CPU) has " << endl;
+}else{
+	cout << "Invalid number of cores entered. Program will terminate " << endl;
+}
 	
 	cin >> numberOfcores;
 	
@@ -24,5 +42,54 @@ int main()
 	cout << "\t 4. 3840 x 2160 \n";
 	
 	cin >> menu;
+	
+	double menuItem1 = 1;  //"1280 x 720";
+	double  menuItem2 = 0.75; //"1920 x 1080";
+	double menuItem3 = 0.55;  // "2560 x 1440";
+	double menuItem4 = 0.35;  //"3840 x 2160";
+	
+	if(menu == 1){
+		mult = menuItem1;
+		
+	}else
+		if(menu == 2){
+		mult = menuItem2;
+		
+	}else
+		if(menu == 3){
+		mult = menuItem3;
+		
+	}else
+		if(menu == 4){
+		mult = menuItem4;
+		
+	}else{
+		//tell user that something went wrong, no input received
+		
+		cout << " ERROR \n"; 
+	}
+	
+	performanceScore = ((5 * speedGpu) + (numberOfcores * speedCpu)) * mult;
+	
+	string text;
+	text = "Computer Hardware Graphics Quality Recommendation Tool \n";
+	
+	cout << text << endl;
+	
+	cout << "GPU clock speed: " << speedGpu << endl;
+	
+	cout << "CPU clock speed: " << speedCpu << endl;
+	
+	cout << "The number of cores: " << numberOfcores << endl;
+	
+	cout << "Monitor Resolution " << menu << endl;
+	
+	cout << setprecision(3) << showpoint << fixed << endl;
+	
+	cout << "The Performance Score: " << performanceScore <<endl;
+	
+	cout << "The recommended Graphics Quality: Unable to Play \n";
+	
+	
 	
 }
